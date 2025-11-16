5088477ebe874567d698e27748db16e99d70772e
Darryl Nicholson <darryl.a.nicholson@gmail.com>
2025-11-16 12:19:00 -0800

    feat: add tab bar icons for profile, books, and create screens

--- app/(dashboard)/_layout.jsx
+++ app/(dashboard)/_layout.jsx
@@ -1,6 +1,7 @@
 import { Tabs } from 'expo-router'
 import { useColorScheme } from 'react-native'
 import { Colors } from '../../constants/Colors'
+import { Ionicons } from '@expo/vector-icons'
 
 const DashBoardLayout = () => {
     const colorScheme = useColorScheme(); // Placeholder for color scheme detection
@@ -19,9 +20,33 @@ const DashBoardLayout = () => {
             tabBarInactiveTintColor: theme.iconColor,
         }}
     >
-    <Tabs.Screen name="profile" options={{ title: 'Profile' }} />
-    <Tabs.Screen name="books" options={{ title: 'Books' }} />
-    <Tabs.Screen name="create" options={{ title: 'Create' }} />
+    <Tabs.Screen name="profile" options={{ title: 'Profile', tabBarIcon: ({focused}) => (
+        <Ionicons 
+            name={focused ? "person" : "person-outline"}
+            size={24}
+            color={focused ? theme.iconColorFocused : theme.iconColor}
+        />
+        )}}
+    />
+    <Tabs.Screen
+        name="books"
+        options={{ title: 'Books', tabBarIcon: ({focused}) => (
+            <Ionicons 
+                name={focused ? "book" : "book-outline"}
+                size={24}
+                color={focused ? theme.iconColorFocused : theme.iconColor}
+            />
+        ) }}
+    />
+    <Tabs.Screen 
+    name="create" 
+    options={{ title: 'Create', tabBarIcon: ({focused}) => (
+        <Ionicons 
+            name={focused ? "create" : "create-outline"}
+            size={24}
+            color={focused ? theme.iconColorFocused : theme.iconColor}
+        />
+    ) }} />
     </Tabs>
   )
 }
