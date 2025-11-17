c9e916686ad6d76e03609eceb52899639fb1a7c8
Darryl Nicholson <darryl.a.nicholson@gmail.com>
2025-11-17 11:57:58 -0800

    feat: add UserContext for authentication management

---
+++ contexts/UserContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useState } from "react"
+
+export const UserContext = createContext(null);
+
+export function UserProvider({children}) {
+    const [user, setUser] = useState(null);
+
+    async function login(email, password) {
+
+    }
+
+    async function register(email, password) {
+
+    }
+    
+    async function logout() {
+
+    }
+
+    return (
+        <UserContext.Provider value={{user, setUser, login, register, logout}}>
+            {children}
+        </UserContext.Provider>
+    )
+}
